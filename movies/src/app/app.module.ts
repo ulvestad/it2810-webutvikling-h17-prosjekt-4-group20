9fc6f0ba3e7a3066cd841536960ab379e6d6edf1
Ole-Martin Skagevang <hero3@hotmail.com>
2017-10-25 13:55:12 +0200

    fixed routing (#13)

--- movies/src/app/app.module.ts
+++ movies/src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  /*add usercomponent here! Not under path: '**' */
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +20,11 @@ import { RegisterComponent } from './components/register/register.component';
     RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- True for debugging
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
