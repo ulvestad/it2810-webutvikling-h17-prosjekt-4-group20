62ee724c871642586c22a55ad173f46f7f793dd0
Kristian Aurlien <kristian@aurlien.net>
2017-10-26 12:42:01 +0200

    remove whitespace

--- movies/server.js
+++ movies/server.js
@@ -26,7 +26,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 
-
 //Set Port
 const port = process.env.PORT || '3000'
 app.set('port', port)
@@ -35,4 +34,4 @@ const server = http.createServer(app)
 
 server.listen(port, () => console.log(`Running on localhost:${port}`))
 
-module.exports = serverg
+module.exports = server
