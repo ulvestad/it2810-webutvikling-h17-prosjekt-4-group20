d0a89dd6aa6da045d1e1382c110efa4f69d2a483
Andreajl <andreajl@stud.ntnu.no>
2017-10-27 12:38:25 +0200

    auth with bcrypt + jasonwebtoken

--- movies/server/config.js
+++ movies/server/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   'secret': 'sup3r-s3cr3t',
-  'local_url': 'http://localhost:27017'
+  'database': 'mongodb://localhost/dev',
+  'port': process.env.PORT || 3000
 }
--- movies/server/models/user.js
+++ movies/server/models/user.js
@@ -1,8 +1,34 @@
-var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
+'use strict'
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const Schema = mongoose.Schema
 
-module.exports = mongoose.model('User', new Schema({
-  userid: String,
-  email: String,
-  hash: String,
-}));
+/* User schema */
+let UserSchema = new Schema({
+  userid: {
+  	type: String,
+  	unique: true,
+  	trim: true,
+  	required: true
+  },
+  email: {
+  	type: String,
+  	trim: true,
+  	required: true
+  },
+  hash: {
+  	type: String,
+  	required: true
+  }
+})
+
+// XXX put in userschme.pre for hasing? instead of keeping it the controller?
+
+/* Compare given password with saved hash*/
+UserSchema.methods.comparePasswords = (password, hash, callback) => {
+	bcrypt.compare(password, hash, callback)
+}
+
+mongoose.model('User', UserSchema)
+
+
