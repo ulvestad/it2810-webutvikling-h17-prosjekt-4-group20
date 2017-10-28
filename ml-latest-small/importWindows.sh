33e299bec7b2130e154bc83d963748384956a1e1
ulvestad <simenu95@gmail.com>
2017-10-28 16:07:06 +0200

    database instructions for windows compatibility

---
+++ ml-latest-small/importWindows.sh
@@ -0,0 +1,4 @@
+./mongoimport -d dev -c movies --type csv --file movies.csv --fields movieId,title,genres
+./mongoimport -d dev -c links --type csv --file links.csv --fields movieId,imdbId,tmdbId
+
+./mongo && use movies
