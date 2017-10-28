6050156a0471d2976c2759b35f9b3dedda23d382
Andreajl <andreajl@stud.ntnu.no>
2017-10-28 13:41:08 +0200

    not done fixing reg, wip

--- movies/src/app/app.module.ts
+++ movies/src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
+import { FormsModule }   from '@angular/forms';
 
 import { DataService } from './services/data.service';
+import { PasswordValidatorDirective } from './directive/password-validator.directive';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -12,6 +14,7 @@ import { HomeComponent } from './components/home/home.component';
 import { UserComponent } from './components/user/user.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
+
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -30,11 +33,13 @@ const appRoutes: Routes = [
     HomeComponent,
     UserComponent,
     NavBarComponent,
-    MovieListComponent
+    MovieListComponent,
+    PasswordValidatorDirective
   ],
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: false } // <-- True for debugging
--- movies/src/app/components/register/register.component.ts
+++ movies/src/app/components/register/register.component.ts
@@ -1,7 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { NgModule, Component, OnInit, ViewChild } from '@angular/core';
+import { FormsModule, FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
 import { DataService } from '../../services/data.service';
+
+class User {
+  constructor(
+    public userid: string = '',
+    public email: string = '',
+    public password: string = '',
+    public confirm: string = '') {
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -10,17 +23,31 @@ import { DataService } from '../../services/data.service';
 })
 export class RegisterComponent implements OnInit {
 
-  private results: any;
+  private result: string; // result from server
+  private user: User = new User() // input data
+  @ViewChild('f') form: any; // the form
 
   constructor(private dataService: DataService, private router: Router) { }
 
-  ngOnInit() {}
-
-  register(userid:string, email:string, password:string, confirm:string){
-    this.dataService.post('/register', {userid, email, password, confirm}).subscribe(data => {
-    	if (data.success) this.router.navigate(['login']); // success
-      else this.results = data; // fail
-    })
+  ngOnInit() { 
+    this.form.reset();
   }
 
+  onSubmit(form: any): void {
+    this.user = {...form}
+    // TODO Confirm password
+    this.dataService.post('/register', this.user).subscribe(data => {
+      if (data.success) { // success
+        this.router.navigate(['login']); // success
+        this.form.reset();
+      } else { // fail
+        this.result = data.msg
+        if (data.msg === 'err, user exists') { // TODO change error msg
+          this.user.userid = '';
+        } else if (data.msg === 'email in use') { // TODO fix this on server
+          this.user.email = '';
+        }
+      }
+    })
+  }
 }
