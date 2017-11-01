3490863be57f2a96c96f10ab2883a89bbfb4dff7
ulvestad <simenu95@gmail.com>
2017-11-01 12:08:18 +0100

    user things

--- movies/src/app/components/home/home.component.ts
+++ movies/src/app/components/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
 export class HomeComponent implements OnInit {
 
   movies: Array<any>;
-  moviePosters: Array<any>;
+  moviePostersTop4: Array<any>; // Top 4 trending
+  moviePostersTop4_12: Array<any>; // Next 8 top-trending
+  moviePostersTop12_20: Array<any>; // Next 8...
 
   constructor(private dataService: DataService) {
 
@@ -19,8 +22,14 @@ export class HomeComponent implements OnInit {
     document.body.style.backgroundImage = 'none';
     document.body.style.backgroundColor = '#fff';
 
-    //only for testing
-    this.moviePosters = [
+    //only for testing---------------------------------------------------------------------
+    this.moviePostersTop4 = [
+      'https://i.pinimg.com/236x/b2/b8/e5/b2b8e5daddd56ccf4d9846994160e915.jpg',
+      'https://i.pinimg.com/236x/6d/2e/d5/6d2ed55a3577cde817203baaa943adad.jpg',
+      'https://i.pinimg.com/236x/86/bf/c9/86bfc9793263d10383eca103ae59eee6.jpg',
+      'https://i.pinimg.com/236x/c9/33/18/c933181df3a46b1cc3f34140f13ef9d9--christopher-nolan-minimalist-movie-posters.jpg'
+    ]
+    this.moviePostersTop4_12 = [
       'https://i.pinimg.com/236x/c9/33/18/c933181df3a46b1cc3f34140f13ef9d9--christopher-nolan-minimalist-movie-posters.jpg',
       'https://i.pinimg.com/236x/87/08/6b/87086bde97144f194d94c50c2fec40d8--arno-digital-illustration.jpg',
       'https://i.pinimg.com/236x/1e/76/d2/1e76d2b4ed7f79c410b9b773f8b730d8--cinema-posters-movie-posters.jpg',
@@ -29,6 +38,8 @@ export class HomeComponent implements OnInit {
       'https://i.pinimg.com/236x/31/4e/ba/314eba65af78e70ae1b09bd45ac65bef--art-movies-alternative-movie-posters.jpg',
       'https://i.pinimg.com/236x/31/a8/0a/31a80a23e1996b8ef8480120c99548d7--poster-film-movie-posters.jpg',
       'https://i.pinimg.com/236x/c7/ab/5c/c7ab5c0a178c8e476696861279c910a7--terminator--terminator-poster.jpg',
+    ]
+    this.moviePostersTop12_20 = [
       'https://i.pinimg.com/236x/e7/49/7e/e7497ea0bd84e928d506c44de7d15378--alternative-movie-posters-poster-art.jpg',
       'https://i.pinimg.com/236x/49/41/f7/4941f7314028a75844c6d6b5367b1d6d--alan-turing-game-.jpg',
       'https://i.pinimg.com/236x/7a/a0/b2/7aa0b2af08708899072ab4df64e5caac--ant-man-poster-designs.jpg',
@@ -36,8 +47,9 @@ export class HomeComponent implements OnInit {
       'https://i.pinimg.com/236x/b2/b8/e5/b2b8e5daddd56ccf4d9846994160e915.jpg',
       'https://i.pinimg.com/236x/6d/2e/d5/6d2ed55a3577cde817203baaa943adad.jpg',
       'https://i.pinimg.com/236x/86/bf/c9/86bfc9793263d10383eca103ae59eee6.jpg',
-      'https://i.pinimg.com/236x/c9/33/18/c933181df3a46b1cc3f34140f13ef9d9--christopher-nolan-minimalist-movie-posters.jpg',
+      'https://i.pinimg.com/236x/c9/33/18/c933181df3a46b1cc3f34140f13ef9d9--christopher-nolan-minimalist-movie-posters.jpg'
     ]
+    //only for testing---------------------------------------------------------------------
 
     this.dataService.getMovies()
       .subscribe((res) => {
